7911b84b240377a4252b24e6109484a23ba911d5
gotczmanmark12 <gotczmanmark69@gmail.com>
2026-04-10 21:13:18 +0300

    data refresh

--- js/script.js
+++ js/script.js
@@ -26,42 +26,5 @@ window.addEventListener("scroll", function () {
   }
 });
 
-const counters = document.querySelectorAll('.counter');
 
-let started = false;
 
-function runCounters() {
-
-  counters.forEach(counter => {
-    const target = +counter.getAttribute('data-target');
-    let count = 0;
-
-    const update = () => {
-      const increment = target / 100;
-
-      if (count < target) {
-        count += increment;
-        counter.innerText = Math.ceil(count);
-        setTimeout(update, 20);
-      } else {
-        counter.innerText = target;
-      }
-    };
-
-    update();
-  });
-}
-
-// csak amikor odaérsz scrollal
-window.addEventListener("scroll", () => {
-
-  const statsSection = document.querySelector(".stats");
-  const sectionTop = statsSection.offsetTop;
-  const screenHeight = window.innerHeight;
-
-  if (!started && window.scrollY > sectionTop - screenHeight + 100) {
-    runCounters();
-    started = true;
-  }
-
-});
